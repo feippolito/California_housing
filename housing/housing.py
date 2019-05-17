e7aa4561c36750c3b1115cc79fc11db9a2cb6039
unknown <fippolito@EEL-26336.maua.br>
2019-05-17 15:28:39 -0300

    drop columns working on class, not working with html

--- housing/housing.py
+++ housing/housing.py
@@ -4,9 +4,6 @@
 # dataset:
 # http://www.dcc.fc.up.pt/~ltorgo/Regression/cal_housing.html
 
-# In[13]:
-
-
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -15,24 +12,18 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import NearestNeighbors
 
 
-# In[14]:
-
-
 class Housing:
     def __init__(self):
         self.housing = pd.read_csv('housing.csv',sep=',',decimal='.',encoding='utf-8')
-        self.housing_encoded = self.encode_fit_transform(self.housing)
-        
-        self.findKneighbors_fit(self.housing_encoded)
-        
+
     def encode_fit_transform(self, database):
         
-        #one hot encoder for ocean_proximity
+        # one hot encoder for ocean_proximity
         self.onehot = OneHotEncoder(sparse=False, categories= 'auto')
         ocean_labels=database.ocean_proximity.values.reshape(-1, 1)
         onehot_ocean_labels = self.onehot.fit_transform(ocean_labels)
         
-        #concatenate onehot encoded values to dataframe
+        # concatenate onehot encoded values to dataframe
         dfOneHot = pd.DataFrame(onehot_ocean_labels,
                                 columns = ["Ocean_"+str(int(i)) for i in range(onehot_ocean_labels.shape[1])])
         encoded_database = pd.concat([database, dfOneHot], axis=1)
@@ -47,54 +38,58 @@ class Housing:
         #normilize values all columns
         self.scaler = MinMaxScaler()
         encoded_database[self.columns] = self.scaler.fit_transform(encoded_database[self.columns])
-        
         return encoded_database
     
     def findKneighbors_fit(self, database):
         self.nNeighbors = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(database)        
     
-    def transform(self, sample):
-        columns_ = ['longitude', 'latitude', 'housing_median_age', 'total_rooms',
-                    'total_bedrooms', 'population', 'households', 'median_income',
-                    'median_house_value', 'ocean_proximity']
-
-        #transform list to dataframe
-        sample_df = pd.DataFrame(columns = columns_)
-        sample_df.loc[0] = sample
+    def transform(self, sample_df):
         
         #transform
         #onehot encoder
-        ocean_labels=sample_df.ocean_proximity.values.reshape(-1, 1)
+        ocean_labels=self.sample_df.ocean_proximity.values.reshape(-1, 1)
         onehot_ocean_labels = self.onehot.transform(ocean_labels)
         
         #concatenate onehot encoded values to dataframe
         dfOneHot = pd.DataFrame(onehot_ocean_labels,
                                 columns = ["Ocean_"+str(int(i)) for i in range(onehot_ocean_labels.shape[1])])
-        encoded_sample = pd.concat([sample_df, dfOneHot], axis=1)
-        
+        encoded_sample = pd.concat([self.sample_df, dfOneHot], axis=1)
         encoded_sample.drop('ocean_proximity', axis = 1, inplace = True) #drop ocean_proximity original column
-        
-        
         encoded_sample[self.columns] = self.scaler.transform(encoded_sample[self.columns]).astype(float)
         
         return encoded_sample
     
     def findKneighbors(self,sample, n_kneighbors = 5):
-        self.encoded_sample = self.transform(sample)
-        dist, ind = self.nNeighbors.kneighbors(self.encoded_sample, n_kneighbors)
-        return self.housing.loc[ind[0].tolist()]
+        self.housing_ = self.housing.copy(deep = True)
+        n_none = [i for i in range(len(sample)) if sample[i] == None]
         
+        # Transform list to dataframe
+        columns_ = ['longitude', 'latitude', 'housing_median_age', 'total_rooms',
+                    'total_bedrooms', 'population', 'households', 'median_income',
+                    'median_house_value', 'ocean_proximity']
 
+        for index in sorted(n_none, reverse=True):
+            del columns_[index]
+            del sample[index]
+        
+        # transform list to dataframe
+        self.sample_df = pd.DataFrame(columns = columns_)
+        self.sample_df.loc[0] = sample
+        
+        if len(n_none) != 0:
+            #self.sample_df.drop(self.sample_df.columns[n_none],axis=1,inplace=True)
+            self.housing_.drop(self.housing_.columns[n_none],axis=1, inplace=True)
+        
+        self.housing_encoded = self.encode_fit_transform(self.housing_)
+        self.findKneighbors_fit(self.housing_encoded)
+        
+        self.encoded_sample = self.transform(self.sample_df)
+        dist, ind = self.nNeighbors.kneighbors(self.encoded_sample, n_kneighbors)
+        
+        return self.housing.loc[ind[0].tolist()]
 
-# house1 = Housing()
+#house1 = Housing()
 
-# datatable1 = house1.findKneighbors([-120.24,
-#  32.85,
-#  54.0,
-#  1467.0,
-#  190.0,
-#  496.0,
-#  177.0,
-#  7.2574,
-#  352100.0,
-#  'NEAR BAY'], 10)
+#datatable1 = house1.findKneighbors([-120.24, 32.85, 54.0, 1467.0, 190.0, 496.0, 177.0, 7.2574, 352100.0, 'NEAR BAY'], 10)
+
+#print(datatable1)
--- housing/web.py
+++ housing/web.py
@@ -8,31 +8,29 @@ app = Flask(__name__)
 @app.route('/housing',methods = ['POST', 'GET'])
 def housing():
     if request.method == 'POST':
-        var_1 = request.form['nm1']
-        var_2 = request.form['nm2']
-        var_3 = request.form['nm3']
-        var_4 = request.form['nm4']
-        var_5 = request.form['nm5']
-        var_6 = request.form['nm6']
-        var_7 = request.form['nm7']
-        var_8 = request.form['nm8']
-        var_9 = request.form['nm9']
-        var_10 = request.form['nm10']
-
-        n_kneighbors = int(request.form['n'])
-
-        fvar_1 = float(var_1)
-        fvar_2 = float(var_2)
-        fvar_3 = float(var_3)
-        fvar_4 = float(var_4)
-        fvar_5 = float(var_5)
-        fvar_6 = float(var_6)
-        fvar_7 = float(var_7)
-        fvar_8 = float(var_8)
-        fvar_9 = float(var_9)
         
+        list_var_str = []
+        list_var = []
+
+        list_var_str.append(request.form['nm1'])
+        list_var_str.append(request.form['nm2'])
+        list_var_str.append(request.form['nm3'])
+        list_var_str.append(request.form['nm4'])
+        list_var_str.append(request.form['nm5'])
+        list_var_str.append(request.form['nm6'])
+        list_var_str.append(request.form['nm7'])
+        list_var_str.append(request.form['nm8'])
+        list_var_str.append(request.form['nm9'])
+        
+        for var in list_var_str:
+          if var:
+            list_var.append(float(var))
+          else:
+            list_var.append(0)
+            
+        list_var.append(request.form['nm10'])
+        n_kneighbors = int(request.form['n'])
         house1 = Housing()
-        list_var = [fvar_1, fvar_2, fvar_3, fvar_4, fvar_5, fvar_6, fvar_7, fvar_8, fvar_9, var_10]
 
         df = house1.findKneighbors(list_var, n_kneighbors)
         
